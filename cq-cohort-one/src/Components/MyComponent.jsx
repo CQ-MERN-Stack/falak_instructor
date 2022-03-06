b63b1e3e68e81abb7ac16f0a67e28d1b98daa37c
Falak <falak@hotmail.co.in>
2022-03-06 19:07:58 +0530

    File Structure

---
+++ cq-cohort-one/src/Components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class MyComponent extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>My React App</h1>
+      </>
+    )
+  }
+}
